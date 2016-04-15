09582baf940a82d3f93fb7052cca3808ddec4026
deepwaterooo <heyanh@vandals.uidaho.edu>
2016-04-15 14:12:44 -0700

    for my own references

---
+++ elpa/ac-python-20110519/ac-python-autoloads.el
@@ -0,0 +1,18 @@
+;;; ac-python-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("ac-python-pkg.el" "ac-python.el") (21510
+;;;;;;  35308 396211 847000))
+
+;;;***
+
+(provide 'ac-python-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ac-python-autoloads.el ends here
---
+++ elpa/anything-obsolete-0.1/anything-obsolete-autoloads.el
@@ -0,0 +1,18 @@
+;;; anything-obsolete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("anything-obsolete-pkg.el" "anything-obsolete.el")
+;;;;;;  (21510 35307 46540 285000))
+
+;;;***
+
+(provide 'anything-obsolete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anything-obsolete-autoloads.el ends here
