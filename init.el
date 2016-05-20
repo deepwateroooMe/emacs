b36e7dff09191da0c76fddf9f3cc69cafd9b1187
deepwaterooo <heyanh@vandals.uidaho.edu>
2016-05-19 18:17:22 -0700

    parentheses: in progress

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
-(setq default-directory "~/Documents/summer16/programmingLT/hw1_racket/")
+(setq default-directory "/Users/qunyan/Documents/summer16/programmingLT/plt-git/hw1_racket/")
 ;(setq default-directory "/Users/qunyan/AndroidStudioProjects/tetris/3d/app/src/main/java/dev/ttetris/")
 ;(setq default-directory "/Users/qunyan/AndroidStudioProjects/tetris/3d/")
 ;(setq default-directory "/Users/qunyan/Documents/spring16/java/hw2/")
@@ -16,22 +16,15 @@
 ;(global-set-key "\M-\C-r" 'query-replace)
 ;(global-set-key "\M-h" 'help-command)
 
+(setq c-brace-imaginary-offset 1)
 
 ;;; autopair
 (require 'autopair)
+(defun turn-on-autopair-mode () (autopair-mode 1))
 (autopair-global-mode) ;; enable autopair in all buffers
-
-
-;csharp-mode {}} fix:  ;;; old version of c#
-;(add-hook 'csharp-mode
-;	  #'(lambda ()
-;	      (push ?{
-;		    (getf autopair-dont-pair :comment))))
-
-;(setq skeleton-pair t)
-;(global-set-key "(" 'skeleton-pair-insert-maybe)
-;(global-set-key "[" 'skeleton-pair-insert-maybe)
-;(global-set-key "{" 'skeleton-pair-insert-maybe)
+;(electric-pair-mode 1) ;;; emacs 24
+(show-paren-mode 1)
+(setq show-paren-style 'parenthesis) ; 只高亮括号
 
 
 ;;; autorevert buffer
@@ -45,13 +38,6 @@
   (interactive)
   (yank)
   (call-interactively 'indent-region))
-
-
-;;;for csharp-mode ; {} autoindent
-(defun csharp-autoindent ()
-  (when (and (eq major-mode 'csharp-mode) (looking-back "[;]"))
-    (newline-and-indent)))
-(add-hook 'post-self-insert-hook 'csharp-autoindent)
 
 
 ;让Emacs在保存时自动清除行尾空格及文件结尾空行
@@ -236,9 +222,75 @@
 
 ;;; for csharp-mode
 ;(add-to-list 'load-path "~/.emacs.d/elpa/csharp-mode-20130824.1200") ;拓展文件(插件)目录
+;(require 'csharp-mode)
+
 (add-to-list 'load-path "~/.emacs.d/elpa/csharp-mode") ;拓展文件(插件)目录
 (require 'csharp-mode)
-;(add-hook 'csharp-mode 'my-c++-mode-hook)
+;;;for csharp-mode ; {} autoindent
+(defun csharp-autoindent ()
+  (when (and (eq major-mode 'csharp-mode) (looking-back "[;]"))
+    (newline-and-indent)))
+(add-hook 'post-self-insert-hook 'csharp-autoindent)
+(add-hook 'csharp-mode-hook
+	  #'(lambda ()
+	      (local-set-key (kbd "{") 'cheeso-insert-open-brace)))
+
+; work with autopair for {
+(defun cheeso-looking-back-at-regexp (regexp)
+  "calls backward-sexp and then checks for the regexp.  Returns t if it is found, else nil"
+  (interactive "s")
+  (save-excursion
+    (backward-sexp)
+    (looking-at regexp)))
+
+(defun cheeso-looking-back-at-equals-or-array-init ()
+  "returns t if an equals or [] is immediate preceding. else nil."
+  (interactive)
+  (cheeso-looking-back-at-regexp "\\(\\w+\\b *=\\|[[]]+\\)"))  
+
+(defun cheeso-prior-sexp-same-statement-same-line ()
+  "returns t if the prior sexp is on the same line. else nil"
+  (interactive)
+  (save-excursion
+    (let ((curline (line-number-at-pos))
+          (curpoint (point))
+          (aftline (progn
+                      (backward-sexp)
+                      (line-number-at-pos))) )
+      (= curline aftline))))  
+
+(defun cheeso-insert-open-brace ()
+  "if point is not within a quoted string literal, insert an open brace, two newlines, and a close brace; indent everything and leave point on the empty line. If point is within a string literal, just insert a pair or braces, and leave point between them."
+  (interactive)
+  (cond
+   ;; are we inside a string literan? 
+   ((c-got-face-at (point) c-literal-faces)
+    ;; if so, then just insert a pair of braces and put the point between them
+    (self-insert-command 1)
+					(insert "")) ; this one works great now
+
+   ;; was the last non-space an equals sign? or square brackets?  Then it's an initializer.
+   ((cheeso-looking-back-at-equals-or-array-init)
+    (self-insert-command 1)
+    (insert ";")
+    (backward-char 1)) ; this one works great now
+
+   ;; doesn't cooperate well with autopair
+   ;; else, it's a new scope., 
+   ;; therefore, insert paired braces with an intervening newline, and indent everything appropriately.
+   (t
+    (if (cheeso-prior-sexp-same-statement-same-line)
+    (self-insert-command 1))  ;;; so far only upto here, don't know how to eval & expand {}
+    (insert "") 
+    (c-indent-line-or-region)
+
+;	(self-insert-command 1))
+;   (newline-and-indent)
+;   (eval-last-sexp)
+;   (newline-and-indent)
+;   (c-indent-line-or-region)
+;   (previous-line)
+    )))
 
 
 ;;; for company-mode
