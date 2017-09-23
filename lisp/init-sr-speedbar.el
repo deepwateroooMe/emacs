06d9fd5d3a1790843b37e9408a82506850115f58
water123 <jenny@squarepanda.com>
2017-09-22 23:11:48 -0700

    sr-speedbar ediff snippets

---
+++ lisp/init-sr-speedbar.el
@@ -0,0 +1,34 @@
+;;; ### Speedbar ###
+(setq load-path (cons (expand-file-name "~/.emacs.d/elpa/sr-speedbar-20141004.532") load-path))
+(require 'sr-speedbar)
+
+;(autoload 'sr-speedbar-toggle "sr-speedbar") ; avoid init until used
+(setq speedbar-show-unknown-files t) ; 可以显示所有目录以及文件
+(setq speedbar-use-images nil)       ; Turn off the ugly icons
+(setq sr-speedbar-right-side nil)    ; Left-side pane
+(setq sr-speedbar-auto-refresh nil) ; Don't refresh on buffer changes
+
+;; Nicer fonts for speedbar when in GUI
+(when (window-system)
+  ;; keep monospace buttons, but smaller height
+  (set-face-attribute 'speedbar-button-face nil :height 100)
+
+  ;; change to system default UI font for entries
+  (dolist (face (list 'speedbar-file-face 'speedbar-directory-face
+                      'speedbar-tag-face  'speedbar-selected-face
+                      'speedbar-highlight-face))
+    (if (eq system-type 'darwin) ;; Lucida Grande on OS X
+        (set-face-attribute face nil :family "Lucida Grande" :height 110)
+      (set-face-attribute face nil :family "Droid Sans" :height 100))))
+
+;; No left fringe and half-size right fringe. TODO: Doesn't work
+(add-hook 'speedbar-mode-hook (lambda()
+                                (message "FROM SPEEDBAR HOOK")
+                                (message "window-fringes %S" (window-fringes))
+                                (set-window-fringes nil 0)))
+;(add-to-list 'speedbar-frame-parameters '(left-fringe . 0)) ; doesn't seem to work
+;(setq sr-speedbar-width 50)
+(global-set-key [(f5)] 'speedbar-get-focus)
+(speedbar 1)
+
+(provide 'init-sr-speedbar)
