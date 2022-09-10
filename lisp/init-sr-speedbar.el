8e8303d6425c019341600c7f06887345ab65df94
deepwaterooo <heyan.huang2020@gmail.com>
2022-09-10 07:20:04 -0700

    LOVE MY DEAR COUSIN. better sr-speedbar performance on Windows 10 OS -- getting switched and focussed when F5 pressed

--- lisp/init-sr-speedbar.el
+++ lisp/init-sr-speedbar.el
@@ -88,12 +88,52 @@
 (setq sr-speedbar-width-x 35)
 (setq dframe-update-speed t)        ; prevent the speedbar to update the current state, since it is always changing  
 
+(defun sb/expand-tags ()
+
+  "Expand current `sr-speedbar' buffer file."
+  (interactive)
+  ;; We assume that the speedbar name is the same as the file of the buffer
+  (let* ((current-buffer-name (file-name-nondirectory (buffer-file-name)))
+         (file-point nil)
+         (line-list '()))
+    (with-current-buffer speedbar-buffer
+      ;; Refresh the current speedbar buffer
+      (speedbar-refresh)
+      (goto-char (point-min))
+      (re-search-forward current-buffer-name)
+      (setq file-point (point))
+      ;; This function make the point go backwards so we have to save the location
+      (speedbar-flush-expand-line)
+      (goto-char file-point)
+      ;; We enter the "expanded" attributes
+      (forward-line)
+      (while 
+          ;; Check if we reach another file, or the end of the buffer.
+          (and (not (speedbar-line-file))
+               (not (equal (point) (point-max))))
+        (push (point) line-list)
+        (forward-line))
+      ;; Once we have the point of the main branches, we iterate
+      ;; and expand his content
+      (seq-map (lambda (line)
+                 (goto-char line)
+                 (speedbar-flush-expand-line))
+               line-list))))
+;; Add it to the save-hook
+(add-hook 'after-save-hook 'sb/expand-tags)
+
 (global-set-key (kbd "<f5>") (lambda()  
                                (interactive)
                                (sr-speedbar-toggle)
                                (sr-speedbar-select-window)
                                 ))
-;; (global-set-key [(f5)] 'speedbar-get-focus) ;; 不及上面的效果好，容易自动往右缩进
+;; (sr-speedbar-toggle) ;;; 这里的目录不对，应该是动态的目录才对
+;; (global-set-key [(f5)] (lambda ()
+;;                          (interactive)
+;;                          (speedbar-get-focus)
+;;                          (sr-speedbar-select-window)
+;;                          ))
+                ;; 'speedbar-get-focus) ;; 不及上面的效果好，容易自动往右缩进
 
 (setq speedbar-mode-hook '(lambda ()
                             (interactive)
